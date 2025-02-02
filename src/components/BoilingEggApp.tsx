59a56a8815c52ecb8f0408f326d41a839f38f567
Viktoriia Shevchenko <viktoriiashevchenko@Viktoriias-MacBook-Pro.local>
2025-02-02 19:46:49 +0200

    typo

--- src/components/BoilingEggApp.tsx
+++ src/components/BoilingEggApp.tsx
@@ -43,7 +43,7 @@ const BoilingEggApp: React.FC = () => {
       }}
     >
       <Typography variant="h4" gutterBottom>
-        How would you like your eggs?
+        What’s your perfect boiled egg
       </Typography>
       <motion.div
         animate={{ rotate: isBoiling ? 360 : 0 }}
